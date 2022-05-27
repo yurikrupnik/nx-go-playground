015ad90e60c4bf559f0435535b453d76930b7614
yuri <krupnik.yuri@gmail.com>
2022-05-28 01:07:15 +0300

    changed go app

--- apps/users/api/main.go
+++ apps/users/api/main.go
@@ -14,7 +14,7 @@ import (
 	"nx-go-playground/libs/go/myutils"
 )
 
-type UserResponses struct {
+type UserResponseM struct {
 	Status  int        `json:"status"`
 	Message string     `json:"message"`
 	Data    *fiber.Map `json:"data"`
