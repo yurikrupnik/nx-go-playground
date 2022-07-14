a1554cbf483eb4e957ea2c2185f50a8c97b90734
yuri <krupnik.yuri@gmail.com>
2022-07-14 04:20:20 +0300

    sd

---
+++ apps/rust/my-rust-bin/src/model.rs
@@ -0,0 +1,19 @@
+// use serde::{Deserialize, Serialize};
+//
+// #[derive(Clone, Debug, PartialEq, Eq, Deserialize, Serialize)]
+// pub struct User {
+//     pub name: String,
+//     pub age: i32,
+//     pub email: String,
+//     pub password: String,
+// }
+//
+// // #[derive(Clone, Debug, PartialEq, Eq, Deserialize, Serialize)]
+// // pub struct List {
+// //     list: Vec<User>,
+// // }
+// //
+// // pub struct CreateUserDto {
+// //     pub email: String,
+// //     pub password: String,
+// // }
