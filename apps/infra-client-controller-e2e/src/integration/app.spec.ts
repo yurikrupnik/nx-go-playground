8df869a6b96a685aac55286ad8e14142f9d6bc32
yuri <krupnik.yuri@gmail.com>
2022-07-25 21:06:43 +0300

    fixed e2e

--- apps/infra-client-controller-e2e/src/integration/app.spec.ts
+++ apps/infra-client-controller-e2e/src/integration/app.spec.ts
@@ -8,6 +8,6 @@ describe('infra-client-controller', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome infra-client-controller');
+    getGreeting().contains('Hello world');
   });
 });
--- apps/infra/client-controller/src/App.tsx
+++ apps/infra/client-controller/src/App.tsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div class={styles.App}>
       <header class={styles.header}>
+        <h1>Hello world</h1>
         <img src={logo} class={styles.logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
