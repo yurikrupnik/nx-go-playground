4a73f674dc54ab795b326b5bdb54489d0a3f3214
yuri <krupnik.yuri@gmail.com>
2022-07-27 02:39:17 +0300

    removed 2 workflows

--- apps/users/api/main.go
+++ apps/users/api/main.go
@@ -49,7 +49,7 @@ func main() {
 	apiGroup := app.Group("api")
 	apiGroup1 := app.Group("syu")
 	apiGroup1.Get("/aris", func(ctx *fiber.Ctx) error {
-		return ctx.SendString("ssk")
+		return ctx.SendString("ssks")
 	})
 	apiGroup1.Get("/dom", func(ctx *fiber.Ctx) error {
 		return ctx.SendString("dosmd-s")
