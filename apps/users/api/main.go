d4a2809daa3b4375f46fbf831a17081aae582356
yuri <krupnik.yuri@gmail.com>
2022-07-29 16:35:35 +0300

    updated api

--- apps/users/api/main.go
+++ apps/users/api/main.go
@@ -49,7 +49,7 @@ func main() {
 	apiGroup := app.Group("api")
 	apiGroup1 := app.Group("sdd")
 	apiGroup1.Get("/aris", func(ctx *fiber.Ctx) error {
-		return ctx.SendString("sssks")
+		return ctx.SendString("ssssks")
 	})
 	apiGroup1.Get("/dom", func(ctx *fiber.Ctx) error {
 		return ctx.SendString("dosmd-s")
