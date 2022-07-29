eeb27d029ff4339c2b1c11ee734faf44d07f8051
yuri <krupnik.yuri@gmail.com>
2022-07-29 06:03:00 +0300

    updated api

--- apps/users/api/main.go
+++ apps/users/api/main.go
@@ -47,7 +47,7 @@ func main() {
 	//app.Use(csrf.New()) // todo check it - forbidden post events
 	app.Use(cors.New())
 	apiGroup := app.Group("api")
-	apiGroup1 := app.Group("arssosd")
+	apiGroup1 := app.Group("ds")
 	apiGroup1.Get("/aris", func(ctx *fiber.Ctx) error {
 		return ctx.SendString("ssks")
 	})
